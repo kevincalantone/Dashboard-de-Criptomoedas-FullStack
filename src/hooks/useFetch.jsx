a2ba1acc6d78e331d8d8f0c5687efce27700efd7
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-04-11 15:15:32 -0300

    feat: adicionar custom hook useFetch para consumo de API

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,1 +1,29 @@
 //gerenciar o "carregando" automaticamente.
+
+import { useState, useEffect } from "react";
+import api from '../services/api'
+
+export function useFetch(url){
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                setLoading(true)
+                // O "await" trava a execução aqui até a API responder
+                const response = await api.get(url)
+                setData(response.data)
+            }catch(err){
+                setError(err)
+            }finally{
+                setLoading(false)
+            }
+        }
+        fetchData()
+        
+    }, [url]);
+
+    return {data, loading, error}
+}
