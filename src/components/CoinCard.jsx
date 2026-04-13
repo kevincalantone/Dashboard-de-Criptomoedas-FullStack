4dbcd0401d0faf50acda2279e8766ec945b63783
suporte.ti <kevin.calantone@DOMAIN.SIVA.COM>
2026-04-13 16:39:18 -0300

    refactor: isola listagem de moedas em um componente CoinCard

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -2,9 +2,16 @@
 
 import React from 'react'
 
-const CoinCard = () => {
+const CoinCard = ({coin}) => {
   return (
-    <div>CoinCard</div>
+   <div className="coin-card">
+    <img src={coin.image} alt={coin.name} className='coin-image' />
+    <div className="coin-info">
+      <h3 className='coin-name'>{coin.name}</h3>
+      <p className='coin-price'> U$ {coin.current_price.toLocaleString()}</p>
+    </div>
+    <button className='fav-button'>⭐</button>
+   </div>
   )
 }
 
