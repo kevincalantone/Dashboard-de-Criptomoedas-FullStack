4b57708084ba99cd4b1c8861ba9a054d6233b3b1
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-04-21 23:56:59 -0300

    feat: adicionar componente de Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='loader-container'>
+      <div className="spinner"></div>
+      <p>\buscando dados do mercado...</p>
+    </div>
   )
 }
 
