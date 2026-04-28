397622c829e2179198580e84baea830f757ea822
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-04-27 23:39:55 -0300

    feat: organizar estrutura da Navbar com containers para Logo e Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -6,16 +6,13 @@ const Navbar = () => {
       <div className="navbar-logo">
         <NavLink to="/">CriptoDash</NavLink>
       </div>
+      
       <div className="navbar-links">
-        <NavLink to="/" end>
-          Início
-        </NavLink>
-        <NavLink to="/favorites">
-          Meus Favoritos
-        </NavLink>
+        <NavLink to="/" end>Início</NavLink>
+        <NavLink to="/favorites">Meus Favoritos</NavLink>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
