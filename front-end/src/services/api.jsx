6abbc42e71f4c622925edcbbf39e42d2f9c849e2
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-05-04 00:12:53 -0300

    refactor(services): aponta axios para o servidor local (back-end)

--- front-end/src/services/api.jsx
+++ front-end/src/services/api.jsx
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.coingecko.com/api/v3',
-  headers: {
-    'accept': 'application/json',
-    // O Vite usa import.meta.env para acessar as variáveis
-    'x-cg-demo-api-key': import.meta.env.VITE_CG_API_KEY 
-  }
+ 
+  baseURL: 'http://localhost:3001/api' 
 });
 
 export default api;
